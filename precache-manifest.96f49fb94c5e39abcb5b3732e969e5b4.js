d37eaf370734671ac4ef35ecfa41cde8e7d7bac8
Rakshita Jain <51185686+raksh543@users.noreply.github.com>
2020-09-06 13:43:15 +0530

    Updates

---
+++ precache-manifest.96f49fb94c5e39abcb5b3732e969e5b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0841687787c0bb99ba1abb3719386318",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a767c3168b01449e88bf",
+    "url": "/static/js/2.4ed6ef26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ed6ef26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ece2fc4978135415c48",
+    "url": "/static/js/main.5b783c76.chunk.js"
+  },
+  {
+    "revision": "f78f0647f78e460952b7",
+    "url": "/static/js/runtime-main.65206410.js"
+  },
+  {
+    "revision": "09ea31876af5d4c282255f4fb2e9dc79",
+    "url": "/static/media/smile.09ea3187.png"
+  }
+]);
